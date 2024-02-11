25d063aafcb4c063300946d3e5e2b2f371a2da7e
juan <juanm0677@gmail.com>
2024-02-11 13:34:07 -0400

    mostrando bebidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Container } from "react-bootstrap";
 import Formulario from "./components/Formulario";
 import { CategoriasProvider } from "./context/CategoriasProvider";
+import { BebidasProvider } from "./context/BebidasProvider";
+import ListadoBebidas from "./components/ListadoBebidas";
 function App() {
   return (
     <CategoriasProvider>
-      <header className="py-5">
-        <h1>Buscador de Bebidas</h1>
-      </header>
-      <Container className="mt-5">
-        <Formulario />
-      </Container>
+      <BebidasProvider>
+        <header className="py-5">
+          <h1>Buscador de Bebidas</h1>
+        </header>
+        <Container className="mt-5">
+          <Formulario />
+          <ListadoBebidas  />
+        </Container>
+      </BebidasProvider>
     </CategoriasProvider>
-    
   );
 }
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,27 @@
-import { Button, Form, Row, Col } from "react-bootstrap";
+import { Button, Form, Row, Col, Alert } from "react-bootstrap";
 import useCategorias from "../hooks/useCategorias";
+import { useState } from "react";
+import useBebidas from "../hooks/useBebidas";
 function Formulario() {
+  const [busqueda, setBusqueda] = useState({
+    nombre: "",
+    categoria: "",
+  });
+  const [alerta, setAlerta] = useState("");
   const { categorias } = useCategorias();
+  const {consultarBebida} =useBebidas()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Todos los campos son obligatorios");
+      return
+    }
+    setAlerta('')
+    consultarBebida(busqueda)
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
+      {alerta && <Alert variant="danger"> {alerta} </Alert>}
       <Row>
         <Col md={6}>
           <Form.Group className="mb-3">
@@ -13,6 +31,13 @@ function Formulario() {
               type="text"
               placeholder="Ej: Tequila, Vodka, etc"
               name="nombre"
+              value={busqueda.nombre}
+              onChange={(e) =>
+                setBusqueda({
+                  ...busqueda,
+                  [e.target.name]: e.target.value,
+                })
+              }
             />
           </Form.Group>
         </Col>
@@ -20,7 +45,17 @@ function Formulario() {
           {" "}
           <Form.Group className="mb-3">
             <Form.Label htmlFor="categoria">Categoria Bebida</Form.Label>
-            <Form.Select id="categoria" name="categoria">
+            <Form.Select
+              id="categoria"
+              name="categoria"
+              value={busqueda.categoria}
+              onChange={(e) =>
+                setBusqueda({
+                  ...busqueda,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            >
               <option>-Selecione categoria-</option>
               {categorias.map((categoria) => (
                 <option
@@ -38,7 +73,13 @@ function Formulario() {
       <Row className="justify-content-end">
         <Col md={3}>
           {" "}
-          <Button variant="danger" className="text-uppercase w-100">Buscar Bebidas</Button>{" "}
+          <Button
+            variant="danger"
+            className="text-uppercase w-100"
+            type="submit"
+          >
+            Buscar Bebidas
+          </Button>{" "}
         </Col>
       </Row>
     </Form>
